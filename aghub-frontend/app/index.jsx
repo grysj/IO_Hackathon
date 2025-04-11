95cee5dfe8fcfa2c72e3de6b0a5099117adb034e
Eryk666 <olejarzeryk@gmail.com>
2025-04-11 15:57:00 +0200

    Add gluestack-ui

--- aghub-frontend/app/index.jsx
+++ aghub-frontend/app/index.jsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
     <View
       style={{
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello, krzysiu!</Text>
+      <Text className="text-2xl">Hello, world!</Text>
     </View>
   );
 }
