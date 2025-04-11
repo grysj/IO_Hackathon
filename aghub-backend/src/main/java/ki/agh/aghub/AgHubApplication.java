c40ce9c840b2733b8a10068a4a29c449ce7609b3
Jeremylaby <barycki@student.agh.edu.pl>
2025-04-11 16:52:42 +0200

    agh-backend project spring boot inizialized

---
+++ aghub-backend/src/main/java/ki/agh/aghub/AgHubApplication.java
@@ -0,0 +1,13 @@
+package ki.agh.aghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgHubApplication.class, args);
+	}
+
+}
