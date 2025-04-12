aacf3e896057979eeb00cf25c350d035f2bafd3a
grysj <jgrys17@gmail.com>
2025-04-12 12:07:04 +0200

    small fix

---
+++ aghub-backend/src/main/java/ki/agh/aghub/model/Unavaiability.java
@@ -0,0 +1,25 @@
+package ki.agh.aghub.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name= "unavailabilities")
+public class Unavaiability {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime date_start;
+    private LocalDateTime date_end;
+    private String name;
+    private String description;
+
+}
