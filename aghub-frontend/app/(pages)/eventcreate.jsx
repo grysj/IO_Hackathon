7b119446bef177d9cedd9d0fe5d92427e761b478
Jeremy <barycki@student.agh.edu.pl>
2025-04-12 21:00:17 +0200

    Auto stash before merge of "event-creation-friendslist" and "origin/event-creation-friendslist"

--- aghub-frontend/app/(pages)/eventcreate.jsx
+++ aghub-frontend/app/(pages)/eventcreate.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useRouter } from "expo-router";
 import { Button } from "react-native";
 import { Box } from "@gluestack-ui/themed";
@@ -11,6 +12,24 @@ const EventCreateScreen = () => {
             <Button title="Przejdź do dostępnych terminów" onPress={() => router.push("/(pages)/availability")} />
         </Box>
     );
+=======
+import {useEffect, useState} from "react";
+import {Box, Text} from "@gluestack-ui/themed";
+import FriendSelector from "../../components/event/FriendSelector";
+
+const EventCreateScreen = () => {
+    const [friends, setFriends] = useState([])
+    const onConfirm = (ids) => {
+        setFriends(ids)
+    }
+    return (
+        <Box className="flex-1 bg-background-50">
+            <FriendSelector onConfirm={onConfirm}/>
+            <Text>
+            {`${friends}`}
+            </Text>
+        </Box>)
+>>>>>>> Stashed changes
 };
 
 export default EventCreateScreen;
