611b854bc1f80f9555efe35986d0b9f71510f51f
Krzysztof Swedziol <swedziol@student.agh.edu.pl>
2025-04-12 21:15:55 +0200

    add locationpicker, working, but have to integrate it with rest of event creation proccess

--- aghub-frontend/app/(pages)/eventcreate.jsx
+++ aghub-frontend/app/(pages)/eventcreate.jsx
@@ -1,19 +1,27 @@
-import {useEffect, useState} from "react";
-import {Box, Text} from "@gluestack-ui/themed";
+import { useState } from "react";
+import { Box, Text } from "@gluestack-ui/themed";
 import FriendSelector from "../../components/event/FriendSelector";
+import LocationPickerScreen from "../../components/event/locationpicker";
 
 const EventCreateScreen = () => {
-    const [friends, setFriends] = useState([])
-    const onConfirm = (ids) => {
-        setFriends(ids)
-    }
+    const [friends, setFriends] = useState([]);
+    const [step, setStep] = useState("location");
+
+    const handleFriendsConfirm = (ids) => {
+        setFriends(ids);
+        setStep("location");
+    };
+
     return (
         <Box className="flex-1 bg-background-50">
-            <FriendSelector onConfirm={onConfirm}/>
-            <Text>
-            {`${friends}`}
-            </Text>
-        </Box>)
+            {step === "friends" && (
+                <FriendSelector onConfirm={handleFriendsConfirm} />
+            )}
+            {step === "location" && (
+                <LocationPickerScreen />
+            )}
+        </Box>
+    );
 };
 
 export default EventCreateScreen;
