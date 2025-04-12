613c40fabdd07928c0f5d2964c7f7c29e231e2a7
Krzysztof Swedziol <swedziol@student.agh.edu.pl>
2025-04-12 21:07:04 +0200

    what happened here

--- aghub-frontend/app/(pages)/eventcreate.jsx
+++ aghub-frontend/app/(pages)/eventcreate.jsx
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-import { useRouter } from "expo-router";
-
-const EventCreateScreen = () => {
-    const router = useRouter();
-
-    const handleNavigateToFriends = () => {
-        router.push("/selectfriends");
-    };
-
-    return (
-        <Box className="flex-1 bg-background-50 p-4 justify-center">
-            <Text className="text-lg font-bold mb-4">Tworzenie wydarzenia</Text>
-
-            <Pressable
-                onPress={handleNavigateToFriends}
-                className="bg-primary-500 py-3 px-6 rounded-xl items-center"
-            >
-                <Text className="text-background-light font-bold">Wybierz znajomych</Text>
-            </Pressable>
-        </Box>
-    );
-=======
 import {useEffect, useState} from "react";
 import {Box, Text} from "@gluestack-ui/themed";
 import FriendSelector from "../../components/event/FriendSelector";
@@ -37,7 +14,6 @@ const EventCreateScreen = () => {
             {`${friends}`}
             </Text>
         </Box>)
->>>>>>> Stashed changes
 };
 
 export default EventCreateScreen;
