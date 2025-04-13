ccf07ca523d3987743da6e5d575d939bfa58462a
PiotrBlaszczykk <piotr.blaszczyk.jr@gmail.com>
2025-04-13 11:52:20 +0200

    fetching usos plan, parsing it and giving it to repository

--- aghub-backend/src/main/java/ki/agh/aghub/controller/ClassesController.java
+++ aghub-backend/src/main/java/ki/agh/aghub/controller/ClassesController.java
@@ -2,10 +2,13 @@ package ki.agh.aghub.controller;
 
 import ki.agh.aghub.dto.ClassesDTO;
 import ki.agh.aghub.service.ClassesService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/classes")
@@ -20,6 +23,20 @@ public class ClassesController {
     @PostMapping()
     public void saveClass(@RequestBody ClassesDTO classesDTO) {
         this.classesService.saveClasses(classesDTO);
+    }
+
+
+
+    // Nowy endpoint: przyjmuje url oraz userId do zrobienia planu
+    @PostMapping("/import_usos")
+    public ResponseEntity<String> importUsosPlan(@RequestBody Map<String, String> payload) {
+        String url = payload.get("url");
+        Long userId = Long.parseLong(payload.get("userId"));
+
+        // Wywołujemy nową metodę w service, która ma mapować plan z USOS do DTO
+        classesService.mapUsosPlanToDto(url, userId);
+
+        return ResponseEntity.ok("Import planu z USOS rozpoczęty.");
     }
 }
 
