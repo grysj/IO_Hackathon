13280722600aa80b373bf80fb5fb4b0def5c8e61
Eryk666 <olejarzeryk@gmail.com>
2025-04-14 09:38:34 +0200

    Fix schedule controller

--- aghub-backend/src/main/java/ki/agh/aghub/config/DatabaseSeeder.java
+++ aghub-backend/src/main/java/ki/agh/aghub/config/DatabaseSeeder.java
@@ -1,7 +1,14 @@
 package ki.agh.aghub.config;
 
+import ki.agh.aghub.model.Classes;
+import ki.agh.aghub.model.Event;
 import ki.agh.aghub.model.POI;
+import ki.agh.aghub.model.User;
 import ki.agh.aghub.repository.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -44,16 +51,36 @@ public class DatabaseSeeder {
             poi3.setLng(19.91242912733053);
             poiRepo.save(poi3);
 
+            User user1 = User.builder()
+                .email("asd@asd.asd")
+                .username("asd")
+                .password("password")
+                .build();
+            userRepo.save(user1);
+            
 
-            // klass.setName("AI Fundamentals");
-            // klass.setRoom("Room 101");
-            // klass.setDateStart(LocalDateTime.now().plusDays(1));
-            // klass.setDateEnd(LocalDateTime.now().plusDays(1).plusHours(2));
-            // klass.setPoi(poi);
-            // klass.setUser(user1);
-            // classRepo.save(klass);
+            Classes klass = new Classes();
+            klass.setName("AI Fundamentals");
+            klass.setRoom("Room 101");
+            klass.setDateStart(LocalDateTime.now().plusDays(1));
+            klass.setDateEnd(LocalDateTime.now().plusDays(1).plusHours(2));
+            klass.setPoi(poi);
+            klass.setUser(user1);
+            classRepo.save(klass);
 
             // Create Event
+            Event event = Event.builder()
+                    .latitude(50.06143)
+                    .longitude(19.93658)
+                    .name("AGH Open Day")
+                    .description("An event for prospective students.")
+                    .createdBy(user1)
+                    .poi(poi)
+                    .dateStart(LocalDateTime.now())
+                    .dateEnd(LocalDateTime.now().plusHours(2))
+                    .participants(Set.of(user1))
+                    .build();
+            eventRepo.save(event);
         };
 
     }
