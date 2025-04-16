80b19536f036400d9bfb6e9ae7048834483efd83
Jeremy <barycki@student.agh.edu.pl>
2025-04-16 12:09:39 +0200

    availability picker added new Date picker

---
+++ aghub-frontend/components/event/DateTimePickerBox.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import {Pressable, Text, View} from "react-native";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+
+
+const DateTimePickerBox = ({label ="From", type = "time", value, onChange, visible, setVisible}) => {
+    const setDate = (date) => {
+        if (!date) return;
+
+        switch (type) {
+            case "time": {
+                const newDate = new Date(value);
+                newDate.setHours(date.getHours());
+                newDate.setMinutes(date.getMinutes());
+                onChange(newDate);
+                break;
+            }
+
+            case "date": {
+                const newDate = new Date(date);
+                onChange(newDate);
+                break;
+            }
+
+            default:
+                break;
+        }
+        setVisible(false)
+
+    };
+    const formatText = () => {
+        switch (type) {
+            case "time": {
+                return `${value.getHours().toString().padStart(2, "0")}:${value
+                    .getMinutes()
+                    .toString()
+                    .padStart(2, "0")}`;
+            }
+            case "date": {
+                return `${value.getDate()
+                    .toString()
+                    .padStart(2, "0")}-${value.getMonth()
+                    .toString()
+                    .padStart(2, "0")}-${value.getFullYear()
+                }`
+            }
+        }
+    }
+    return (
+        <View className="flex-1 min-w-[100px]">
+            <Text className="text-white mb-1 font-semibold">{label}:</Text>
+
+            <Pressable onPress={() => setVisible(true)}>
+                <View className="bg-background-200 py-4 rounded-lg">
+                    {visible && (
+                        <RNDateTimePicker
+                            value={new Date()}
+                            mode={type}
+                            display="spinner"
+                            is24Hour={true}
+                            timeZoneName="Europe/Warsaw"
+                            positiveButton={{textColor: "white"}}
+                            negativeButton={{textColor: "white"}}
+                            onChange={(event, date) => {
+                                setDate(date);
+                            }}
+                        />
+                    )}
+
+                    <Text className="text-yellow-600 font-bold text-xl text-center">
+                        {formatText()}
+                    </Text>
+                </View>
+            </Pressable>
+        </View>
+    );
+};
+
+export default DateTimePickerBox;
