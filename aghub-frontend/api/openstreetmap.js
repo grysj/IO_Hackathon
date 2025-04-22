a56461a4fb67c424c71e874984d538c4dfb7b408
Eryk666 <olejarzeryk@gmail.com>
2025-04-22 10:53:38 +0200

    Add necessary headers

--- aghub-frontend/api/openstreetmap.js
+++ aghub-frontend/api/openstreetmap.js
@@ -3,7 +3,14 @@ const API_URL = process.env.EXPO_PUBLIC_OPENSTREETMAP_API_URL;
 export const searchLocation = async (query) => {
   console.log("Searching for location:", query);
   const res = await fetch(
-    `${API_URL}/search?q=${encodeURIComponent(query)}&format=json&limit=1`
+    `${API_URL}/search?q=${encodeURIComponent(query)}&format=json&limit=1`,
+    {
+      method: "GET",
+      headers: {
+        "User-Agent": "AGHub/1.0",
+        "Content-Type": "application/json",
+      },
+    }
   );
 
   console.log(res.status);
